070acaf39a0f009ef91c2b053f5a853af0bc3b3f
Remil <remiliance@yahoo.fr>
2021-06-25 16:50:38 +0200

    der

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../models/Book.model';
+import firebase from 'firebase';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+      firebase.database().ref('/books').set(this.books);
+  }
+
+
+
 }
